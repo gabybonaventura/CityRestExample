6672603bdf063326991ceabc1fc85085bbe6ef4d
Gabriel Bonaventura <gbonaventura@x-28.com>
2017-12-18 12:46:45 -0300

    citydto

---
+++ TodoApi/Models/CityDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi.Models
+{
+    public class CityDto
+    {
+        public CityDto()
+        {
+
+        }
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+        public int NumberOfPointOfInterest
+        {
+            get
+            {
+                return PointOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
+    }
+}
